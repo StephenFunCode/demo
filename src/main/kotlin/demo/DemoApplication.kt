ba9524985b13570f184583f8403c5a350df30e64
Stephen Harman <35758825+StephenFunCode@users.noreply.github.com>
2023-08-06 00:03:16 -0600

    Created basic get endpoint

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,10 +2,20 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
+data class Message(val id: String?, val text: String)
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+	@GetMapping("/")
+	fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
